eeb769291398cf415e5b947a38ef42360832ce1c
MassimaMao <mastem@msn.com>
2022-05-16 00:13:53 +0200

    initiate path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <h1>Hello REACT!</h1>;
-};
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+        explicabo voluptates totam enim sapiente accusantium eos aspernatur
+        laudantium hic perferendis odio cum consequatur, quod libero repellendus
+        id eveniet recusandae molestiae aliquam? Neque quae ea modi quia qui
+        placeat autem numquam repellat minus assumenda ducimus unde soluta, amet
+        alias ad eligendi cum earum recusandae ipsum. Tempora non laboriosam
+        debitis reiciendis hic temporibus id voluptas repellat odit optio
+        laborum at assumenda voluptate, impedit modi quo expedita illo nulla,
+        possimus nam? Impedit nihil dolorum ab, autem hic architecto
+        perspiciatis optio harum consectetur voluptates veniam iure aliquid
+        cumque? Quae magnam ratione eveniet excepturi sint error soluta expedita
+        incidunt assumenda, praesentium nulla tenetur necessitatibus quam vero
+        fugiat aut magni veniam dolorem quis. Neque officia culpa nobis earum
+        quas quaerat minus quam facilis optio, assumenda veniam expedita!
+        Architecto praesentium, tempora quas debitis, id voluptatibus ratione
+        sapiente ullam sequi qui nesciunt rerum, veritatis perspiciatis ea
+        soluta dolore! Dolorem magnam quaerat dolorum repellendus laboriosam eum
+        ipsa, adipisci impedit ut qui atque iste, nisi neque quo voluptatibus
+        totam aperiam provident numquam rem voluptatem veritatis placeat
+        tempora? Odit, modi illo? Possimus quibusdam fuga amet nobis
+        perferendis, commodi, optio inventore rerum, dignissimos minima nulla.
+        Quas veniam corrupti, veritatis sed dignissimos earum, porro officiis
+        rerum quam, eligendi nisi. Velit, perspiciatis eum deserunt inventore,
+        quae ducimus ipsum odio blanditiis sit aspernatur earum aliquam dolore
+        error id optio ut porro quisquam? Aliquam vitae quia eum nostrum enim
+        dolore ea at illo, maiores veniam provident nulla ducimus corporis sint,
+        porro pariatur consequuntur, adipisci libero exercitationem vero magni
+        fuga. Non natus exercitationem nostrum quasi et hic distinctio sunt id
+        nam illum eveniet repellat ratione minima suscipit impedit veniam
+        nesciunt ullam tenetur vero beatae esse, deserunt quod. Deleniti
+        reiciendis, ipsum itaque{' '}
+      </p>
+    </div>
+  );
+}
+
+export default About;
